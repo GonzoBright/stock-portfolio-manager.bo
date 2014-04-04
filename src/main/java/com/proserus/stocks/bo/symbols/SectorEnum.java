37625c1e75cae83f4f355b66f88f73b419d5c59e
Alex <Alex@localhost>
2014-04-03 23:29:32 -0400

    - Mavenize projects - Externalize sectors in a property files - Fix nullpointer when displaying AddTransaction pannel - Add a catchall throwable from swing and log - Fix symbols table not being refreshed in some cases

---
+++ src/main/java/com/proserus/stocks/bo/symbols/SectorEnum.java
@@ -0,0 +1,65 @@
+package com.proserus.stocks.bo.symbols;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.apache.commons.lang3.Validate;
+
+public enum SectorEnum implements Comparable<SectorEnum>{
+	UNKNOWN ("unknown"),
+	MATERIAL ("basicMaterials"),
+	CONGLOMERATES ("conglomerates"),
+	GOODS ("consumerGoods"),
+	FINANCE ("finance"),
+	HEALTHCARE ("healthCare"),
+	INDUSTRY ("industrials"),
+	SERVICES ("services"),
+	ENERGY ("energy"),
+	TECHNOLOGY("technology"),
+	TELECOMMUNICATION("telecommunication"),
+	UTILITIES("utilities"),
+	OTHER("other");
+	
+	private static final ResourceBundle sectors = ResourceBundle.getBundle("sectors", Locale.getDefault());
+	private static final List<SectorEnum> sortedSectors; 
+	
+	static{
+		sortedSectors = Arrays.asList(values());
+		Collections.sort(sortedSectors, new SectorComparator());
+	}
+	
+	
+	
+	private String title = null;
+
+	private SectorEnum(String title){
+		Validate.notNull(title);
+		Validate.notEmpty(title);
+		
+		this.title = title;
+	}
+	
+	public static List<SectorEnum> retrieveSortedSectors(){
+		return sortedSectors;
+	}
+	
+	public String getTitle() {
+		return sectors.getString(title);
+    }
+	
+	@Override
+	public String toString() {
+	    return getTitle();
+	}
+}
+
+class SectorComparator implements Comparator<SectorEnum>{
+	@Override
+	public int compare(SectorEnum o1, SectorEnum o2) {
+		return o1.getTitle().compareTo(o2.getTitle());
+	}
+}
